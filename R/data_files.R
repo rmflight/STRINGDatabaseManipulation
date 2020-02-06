279125c00c1541338e8828515555613ea7f3d43f
rmflight <r.flight79@gmail.com>
2020-02-06 17:17:01 -0500

    smaller data

--- R/data_files.R
+++ R/data_files.R
@@ -1,8 +1,9 @@
-#' STRING V10 detailed links for human
+#' STRING11_9606_links
 #'
 #' Dataset containing the detailed links from v11 of the STRING database for
 #' human. combined_score >= 400, and 10000 randomly chosen links. THIS IS NOT
 #' THE COMPLETE DATASET!
+#'
 #'
 #' @format A data.frame with 10000 rows and 10 variables:
 #' \describe{
@@ -16,20 +17,23 @@
 #'   \item{database}{score for database evidence}
 #'   \item{textmining}{score from textmining}
 #'   \item{combined_score}{total combined score from all evidences}}
+#'
+#'   Processed by RMF
+#'
 #' @source \url{https://stringdb-static.org/download/protein.links.detailed.v11.0/9606.protein.links.detailed.v11.0.txt.gz}
-#' Processed by RMF
 "STRING11_9606_links"
 
-#' STRING V10 aliases for human
+#' STRING11_9606_aliases
 #'
 #' Dataset containing various STRING aliases.
 #'
-#' @format A data.frame with 2449433 rows and 3 variables:
+#' @format A data.frame with 10000 rows and 3 variables:
 #' \describe{
 #'   \item{string}{the STRING ID}
 #'   \item{other}{the other alias}
 #'   \item{type}{the source of the alias (space separated for multiple sources)}}
 #'
-#' @source \url{https://stringdb-static.org/download/protein.aliases.v11.0/9606.protein.aliases.v11.0.txt.gz}
 #' Processed by RMF
+#'
+#' @source \url{https://stringdb-static.org/download/protein.aliases.v11.0/9606.protein.aliases.v11.0.txt.gz}
 "STRING11_9606_aliases"
